029f0b195ac9cad1d5227640bb6dd36ef2ddd94a
Márk Diószegi <walnut5588@gmail.com>
2020-01-06 13:17:48 +0100

    Game Logic

--- game-of-chance-simulator/Program.cs
+++ game-of-chance-simulator/Program.cs
@@ -1,10 +1,32 @@
+using System;
+
 namespace GameOfChanceSimulator
 {
     class Program
     {
         static void Main()
         {
-            System.Console.WriteLine("Epic");
+            Game game = new Game();
+            game.Start();
         }
+
+        /* static bool ()
+        {
+            Random rand = new Random();
+            for (int i = 0; i< 100; i++)   //Esetek száma
+            {
+                double randNum = rand.NextDouble();
+                if (randNum <= 1 * ((double)1 / 100))   //Esély 1 a 100-hoz
+                {
+                    System.Console.WriteLine("Hit!");
+                    return true;
+                }
+                else
+                {
+                    System.Console.WriteLine("Miss.");
+                    return false;
+                }
+            }
+        } */
     }
 }
